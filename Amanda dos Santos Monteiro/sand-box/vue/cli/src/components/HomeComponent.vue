3eb85cb542a9ffb45a37b542aaaa91255397327e
Amanda dos Santos Monteiro <amanda.monteiro12@dinfor.uniceub>
2025-04-09 21:45:36 -0300

    criando nossa HomeComponent

---
+++ Amanda dos Santos Monteiro/sand-box/vue/cli/src/components/HomeComponent.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <footer id="footer">
+            <p>Bem Vindo: {{ nome }} Olá mundo do <b>Vue</b> com CLI</p>
+        </footer>
+    </div>
+</template>
+
+<script>
+export default {
+    name : "HomeComponent",
+    data(){
+        return{
+            nome : "Amanda"
+        }
+    }
+}
+</script>
