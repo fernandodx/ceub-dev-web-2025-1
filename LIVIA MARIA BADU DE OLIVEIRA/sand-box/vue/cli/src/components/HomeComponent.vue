da9ec65740ae1e2de2ef4bbd6e3fb9dd694a2fd9
Lívia Maria Badu de Oliveira <livia.oliveira@dinfor.uniceub>
2025-04-09 21:45:47 -0300

    criando nossa Home Component

---
+++ LIVIA MARIA BADU DE OLIVEIRA/sand-box/vue/cli/src/components/HomeComponent.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <footer id= "footer">
+    </footer>
+    <p>Bem vindo: {{ nome }} Olá mundo <b> Vue com Cli</b></p>
+
+    </div>
+
+</template>
+<script>
+export default {
+    name:"HomeComponent",
+    data() {
+        return {
+            nome: "Livia"
+        }
+    }
+}
+</script>
