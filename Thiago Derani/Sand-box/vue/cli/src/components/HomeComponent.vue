ed87b7ea8b4e49082b38ab768572b08a15ac3296
Thiago Derani Gurgel de Abreu Aquino <thiago.derani@dinfor.uniceub>
2025-04-09 21:47:51 -0300

    Criando HomeComponent

---
+++ Thiago Derani/Sand-box/vue/cli/src/components/HomeComponent.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <footer id="footer">
+            <p>Bem vindo:{{ nome }} Olá mundo do Vue com CLI</p>
+        </footer>
+    </div>
+</template>
+<script>
+export default {
+    name : "HomeComponent",
+    data(){
+        return{
+            nome : "Thiago Derani" 
+        }
+    }
+}
+</script>
