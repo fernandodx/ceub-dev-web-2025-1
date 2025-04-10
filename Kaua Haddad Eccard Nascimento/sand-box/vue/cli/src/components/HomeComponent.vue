5f1a7a91b1c131bb88fce5c4d06ffb85ffe553fc
Kauã Haddad Eccard Nascimento <kaua.nascimento@dinfor.uniceub>
2025-04-09 21:45:39 -0300

    criando nossa HomeComponent

---
+++ Kaua Haddad Eccard Nascimento/sand-box/vue/cli/src/components/HomeComponent.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <footer id="footer">
+           <p>Bem Vindo: {{ nome }} Olá mundo no <b>Vue com CLI</b> </p> 
+        </footer>
+    </div>
+</template>
+
+<script>
+export default {
+    name : "HomeComponent",
+    data() {
+        return{
+            nome : "Kaua"
+        }
+    }
+}
+</script>
