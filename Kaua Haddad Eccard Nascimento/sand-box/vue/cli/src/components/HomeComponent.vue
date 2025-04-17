9cde0f8f78dcc8bc67dea91a710531ca0361d7bb
Kauã Haddad Eccard Nascimento <kaua.nascimento@dinfor.uniceub>
2025-04-16 21:37:20 -0300

    exemplo life circle

--- Kaua Haddad Eccard Nascimento/sand-box/vue/cli/src/components/HomeComponent.vue
+++ Kaua Haddad Eccard Nascimento/sand-box/vue/cli/src/components/HomeComponent.vue
@@ -1,18 +1,18 @@
 <template>
     <div>
         <footer id="footer">
-           <p>Bem Vindo: {{ nome }} Olá mundo no <b>Vue com CLI</b> </p> 
+            <p>Ben Vindo: {{ nome }} Olá mundo do <b>Vue com CLI</b> </p>
         </footer>
     </div>
 </template>
 
 <script>
-export default {
+  export default {
     name : "HomeComponent",
     data() {
-        return{
-            nome : "Kaua"
+        return {
+            nome : "Fernando"
         }
     }
-}
+  }
 </script>
