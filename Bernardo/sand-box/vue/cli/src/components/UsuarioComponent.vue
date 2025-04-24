427ef7808035fd96e316044c8ae472896418370e
Bernardo Alves <bernardoaraujoalves@gmail.com>
2025-04-23 21:39:44 -0300

    implement vue

---
+++ Bernardo/sand-box/vue/cli/src/components/UsuarioComponent.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <p>Nome: {{ nome }}</p>
+        <p>E-mail: {{ email }}</p>
+        <h1 v-show="isFlamengoCampeao">Flamengo Campeão</h1>
+        <br>
+        <button @click="verificarCampeao()">É Campeão</button>
+
+    </div>
+</template>
+
+<script>
+export default {
+    name: "UsuarioComponent",
+    data() {
+        return {
+            nome: "Bernardo",
+            email: "bernardoaraujoalves@gmail.com",
+            isFlamengoCampeao: true
+        }
+    },
+    methods: {
+        verificarCampeao() {
+            this.isFlamengoCampeao = !this.isFlamengoCampeao;
+        }
+    }
+
+}
+
+</script>
