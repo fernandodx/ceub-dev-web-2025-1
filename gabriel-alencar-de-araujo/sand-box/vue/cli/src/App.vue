295313d190392a19fee538bc18fed393eb90bd0c
Gabriel Alencar de Araújo <gabriel.aaraujo@dinfor.uniceub>
2025-04-23 21:49:24 -0300

    aula de diretivas

--- gabriel-alencar-de-araujo/sand-box/vue/cli/src/App.vue
+++ gabriel-alencar-de-araujo/sand-box/vue/cli/src/App.vue
@@ -1,51 +1,58 @@
 <template>
-  <div>
-    <h1>VUE</h1>
-    <br>
-    <home-component />
-    <br>
-    <usuario-component />
-    <br>
-  </div>
-</template>
-
-<script>
-
-import { onUnmounted } from 'vue';
-import HomeComponent from './components/HomeComponent.vue';
-import UsuarioComponent from './components/UsuarioComponent.vue';
-
-export default {
-  name: 'App',
-  components: {
-    HomeComponent,
-    UsuarioComponent
-  },
-  created() {
-    console.log("created");
-  },
-  mounted() {
-    console.log("mounted");
-  },
-  updated() {
-    console.log("updated")
-  },
-  errorCaptured() {
-    console("errorCaptured");
-  },
-  unmounted() {
-    console.log("unmounted");
+    <div>
+      <h1>VUE</h1>
+      <br>
+      <home-component />
+      <br>
+      <usuario-component />
+      <br>
+      <imagem-component />
+      <br>
+      <estilo-component/>
+    </div>
+    
+  </template>
+  
+  <script>
+  
+  import HomeComponent from './components/HomeComponent.vue';
+  import UsuarioComponent from './components/UsuarioComponent.vue';
+  import ImagemComponent from './components/ImagemComponent.vue';
+  import EstiloComponent from './components/EstiloComponent.vue';
+  export default {
+    name: 'App',
+    components: {
+      HomeComponent,
+      UsuarioComponent,
+      ImagemComponent,
+      EstiloComponent
+    },
+    created() {
+      console.log("created");
+    },
+    mounted() {
+      console.log("mounted");
+    },
+    updated() {
+      console.log("updated")
+    },
+    errorCaptured() {
+      console("errorCaptured");
+    },
+    unmounted() {
+      console.log("unmounted");
+    }
   }
-}
-</script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: start;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  </script>
+  
+  <style>
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: start;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+  </style>
+  
