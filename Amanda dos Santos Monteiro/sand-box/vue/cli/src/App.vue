5a25f61e8a253891976a928c0a2ff86d3b0b890d
Amanda dos Santos Monteiro <amanda.monteiro12@dinfor.uniceub>
2025-04-23 21:53:44 -0300

    Imagem

--- Amanda dos Santos Monteiro/sand-box/vue/cli/src/App.vue
+++ Amanda dos Santos Monteiro/sand-box/vue/cli/src/App.vue
@@ -5,6 +5,10 @@
     <home-component/>
     <br>
     <usuario-component/>
+    <br>
+    <imagem-component/>
+    <br>
+    <estilo-component/>
   </div>
     
 </template>
@@ -13,11 +17,15 @@
 
 import HomeComponent from './components/HomeComponent.vue';
 import UsuarioComponent from './components/UsuarioComponent.vue';
+import ImagemComponent from './components/ImagemComponent.vue';
+import EstiloComponent from './components/EstiloComponent.vue';
 export default {
   name: 'App', 
   components: {
     HomeComponent,
-    UsuarioComponent
+    UsuarioComponent,
+    ImagemComponent,
+    EstiloComponent
   },
   created(){
     console.log("created");
---
+++ Amanda dos Santos Monteiro/sand-box/vue/cli/src/components/EstiloComponent.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <h1>Título Padrão</h1>
+        <p class="subtitulo"> Subtítulo estiloso</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "EstiloComponent"
+    }
+</script>
+
+<style scoped>
+
+h1 {
+    color: palevioletred
+}
+
+.subtitulo{
+    text-align: center;
+    font-family: 'Gill Sans';
+    color: brown;
+}
+
+</style>
