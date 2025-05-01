79998b62d851046818383c89e6787557218cfb37
Lívia Maria Badu de Oliveira <livia.oliveira@dinfor.uniceub>
2025-04-30 21:05:46 -0300

    aulas

--- LIVIA MARIA BADU DE OLIVEIRA/sand-box/vue/cli/src/App.vue
+++ LIVIA MARIA BADU DE OLIVEIRA/sand-box/vue/cli/src/App.vue
@@ -1,23 +1,48 @@
 <template>
   <div>
     <h1>VUE</h1>
-  <home-component/>
-
-    
+    <br>
+    <home-component/>
+    <br>
+    <imagem-component urlImagem="https://fly.metroimg.com/upload/q_85,w_700/https://uploads.metroimg.com/wp-content/uploads/2025/04/29101537/Facundo-Torres-e-Estevao-comemoram-na-vitoria-do-Palmeiras-1-2.jpg"/>
+    <imagem-component :urlImagem="urlBanner"/>
+    <br>
+    <estilo-component/>
   </div>
-  </template>
+</template>
 
 <script>
-import HomeComponent from './components/HomeComponent.vue';
-
+import HomeComponent from './components/HomeComponent.vue'
+import ImagemComponent from './components/ImagemComponent.vue';
+import EstiloComponent from './components/EstiloComponent.vue';
 
 export default {
   name: 'App',
   components: {
-    HomeComponent
-    
-    
+    HomeComponent,
+    ImagemComponent,
+    EstiloComponent
+  },
+  data() {
+    return {
+      urlBanner : "https://static.botafogo.com.br/upload/noticia/bafb5c2df19a43388d82792494ddaa10.jpg"
     }
+  },
+  created() {
+    console.log("created");
+  },
+  mounted() {
+    console.log("mounted");
+  },
+  updated(){
+    console.log("updated");
+  },
+  errorCaptured() {
+    console.log("errorCaptured");
+  },
+  unmounted(){
+    console.log("unmounted");
+  }
 }
 </script>
 
@@ -26,7 +51,7 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  text-align: start;
   color: #2c3e50;
   margin-top: 60px;
 }
