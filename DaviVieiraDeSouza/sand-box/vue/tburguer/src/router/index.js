a7476a46f9a89a9980445f9466dd19c6ee72b4a6
Davi Vieira de Souza <davi.vieiras@dinfor.uniceub>
2025-05-21 21:52:11 -0300

    Inicio Projeto TBurguer

---
+++ DaviVieiraDeSouza/sand-box/vue/tburguer/src/router/index.js
@@ -0,0 +1,31 @@
+import { createRouter, createWebHistory } from "vue-router";
+import MenuView from "@/views/MenuView.vue";
+import PedidosView from "@/views/PedidosView.vue";
+
+
+const routes = [
+  {
+    path: "/",
+    name: "home",
+    component: MenuView,
+  },
+  {
+    path: "/pedidos",
+    name: "pedidos",
+    component: PedidosView,
+
+  },
+  {
+    path: "/menu",
+    name: "menu",
+    component: MenuView,
+
+  },
+];
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes,
+});
+
+export default router;
