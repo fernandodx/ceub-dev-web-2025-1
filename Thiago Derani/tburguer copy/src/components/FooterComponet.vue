72dd600bb5f0c4790db7286dd96eabe50f854ed8
Thiago Derani Gurgel de Abreu Aquino <thiago.derani@dinfor.uniceub>
2025-05-28 21:54:18 -0300

    atualizações site

---
+++ Thiago Derani/tburguer copy/src/components/FooterComponet.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <footer id="footer">
+            <p>Construido por Thiago Derani &copy; 2025</p>
+        </footer>
+
+    </div>
+</template>
+<script>
+   export default{
+    name: "Footer Componet"
+   }
+
+</script>
+
+<style scoped>
+
+#footer{
+    height: 80px;
+    background: #333;
+    color: gold;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-weight: 900;
+}
+#footer p {
+    font-size: 14px;
+}
+
+</style>
