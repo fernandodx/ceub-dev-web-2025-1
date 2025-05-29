58407a064680a458478bf7b6446e8961c51bb57e
Lívia Maria Badu de Oliveira <livia.oliveira@dinfor.uniceub>
2025-05-28 21:51:41 -0300

    banner

---
+++ LIVIA MARIA BADU DE OLIVEIRA/tburguer/src/components/FooterComponent.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <footer id="footer">
+            <p> Construido por Livia Badu &copy;2025</p>
+
+        </footer>
+
+    </div>
+
+</template>
+<script>
+
+export default {
+    name: "FooterComponent"
+
+}
+</script>
+
+<style scoped>
+#footer{
+    height: 80px;
+    background: black;
+    color: gold;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-weight: 700;
+}
+
+</style>
