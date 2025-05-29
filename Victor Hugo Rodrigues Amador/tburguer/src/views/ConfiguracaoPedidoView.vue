384658aa908d10e93ef349e85b0153b20236552c
Victor Hugo Rodrigues Amador <victor.amador@dinfor.uniceub>
2025-05-28 21:55:31 -0300

    part 2 projeto t-burguer

---
+++ Victor Hugo Rodrigues Amador/tburguer/src/views/ConfiguracaoPedidoView.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h1>Configuração</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        name : "ConfiguracaoPedidoView"
+    }
+</script>
+
+<style scoped>
+
+</style>
