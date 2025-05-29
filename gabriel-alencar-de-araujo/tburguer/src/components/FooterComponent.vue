a55c2f8dfcf9150ea9feefcd7ead0345c0d451cb
Gabriel Alencar de Araújo <gabriel.aaraujo@dinfor.uniceub>
2025-05-28 21:50:09 -0300

    dando forma na página

---
+++ gabriel-alencar-de-araujo/tburguer/src/components/FooterComponent.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <footer id="footer">
+            <p>Construído por Gabriel Alencar &copy; 2025</p>
+        </footer>
+    </div>
+
+</template>
+<script>
+    export default {
+        name: "FooterComponent"
+    }
+</script>
+
+<style scoped>
+
+#footer {
+    height: 80px;
+    background: #333;
+    color: gold;
+    font-weight: bold;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+#footer p{
+    font-size: 14px;
+}
+</style>
---
+++ gabriel-alencar-de-araujo/tburguer/src/components/NavBarComponent.vue
@@ -0,0 +1,61 @@
+<template>
+    <div>
+      <nav id="nav">
+        <div id="nav-left">
+          <img src="/img/logo_tburguer.png" id="logo">
+        </div>
+        <div id="nav-right">
+          <router-link to="/menu">Menu</router-link>
+          <router-link to="/pedidos">Pedidos</router-link>
+        </div>
+      </nav>
+    </div>
+  </template>
+  
+
+<script>
+    export default {
+        name: "NavBarComponent"
+    }
+</script>
+
+<style scoped>
+
+#nav {
+  background-color: #333;
+  border-bottom: 3px solid darkgoldenrod;
+  padding: 15px 50px;
+  display: flex;
+  justify-content: space-between; /* separa esquerda e direita */
+  align-items: center;
+}
+
+#nav-left {
+  display: flex;
+  align-items: center;
+}
+
+#nav-right {
+  display: flex;
+  align-items: center;
+}
+
+#nav a {
+  color: darkgoldenrod;
+  text-decoration: none;
+  margin: 12px;
+  transition: 0.5s;
+}
+
+#nav a:hover {
+  color: antiquewhite;
+  font-size: 20px;
+}
+
+#logo {
+  width: 50px;
+  height: 50px;
+}
+
+
+</style>
