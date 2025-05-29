530e0580fce3d735a3150c97ffd09771e295efa3
Kauã Haddad Eccard Nascimento <kaua.nascimento@dinfor.uniceub>
2025-05-28 21:55:10 -0300

    parte 2 projeto tburguer

--- Kaua Haddad Eccard Nascimento/tburguer/src/router/index.js
+++ Kaua Haddad Eccard Nascimento/tburguer/src/router/index.js
@@ -1,25 +1,28 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import MenuView from '@/views/MenuView.vue';
+import PedidosView from '@/views/PedidosView.vue';
 
 const routes = [
   {
     path: '/',
     name: 'home',
-    component: HomeView
+    component: MenuView,
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
-]
+    path: '/menu',
+    name: 'menu',
+    component: MenuView,
+  },
+  {
+    path: '/pedidos',
+    name: 'pedidos',
+    component: PedidosView,
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes
-})
+  routes,
+});
 
 export default router
