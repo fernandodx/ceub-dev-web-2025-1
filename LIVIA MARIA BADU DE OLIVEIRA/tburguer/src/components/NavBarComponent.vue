54ba96c3a7cbddfb4fa1b84f61a6c20c33ec833c
Lívia Maria Badu de Oliveira <livia.oliveira@dinfor.uniceub>
2025-06-04 21:57:26 -0300

    enu

--- LIVIA MARIA BADU DE OLIVEIRA/tburguer/src/components/NavBarComponent.vue
+++ LIVIA MARIA BADU DE OLIVEIRA/tburguer/src/components/NavBarComponent.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <nav id="nav">
-            <router-link to="/">
+            <router-link to="/" id="logo-url">
                 <img src="/img/logo_tburguer.png" id="logo"/>
             </router-link>
             <router-link to="/menu">Menu</router-link>
