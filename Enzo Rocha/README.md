fce0178904c5c43144a4de3c444a58d693d02132
enzorocha19 <enzo.rocha@sempreceub.com>
2025-07-01 22:18:58 -0300

    Projeto Final - Enzo Rocha

---
+++ Enzo Rocha/README.md
@@ -0,0 +1,29 @@
+# tburguer
+
+## Project setup
+```
+npm install
+npm install vue-router@4
+npm install -g json-server
+
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
+
+
---
+++ Enzo Rocha/src/components/MessageComponent.vue
@@ -0,0 +1,47 @@
+<template>
+  <div v-show="msg" :class="`message ${type}`">
+    <span>{{ msg }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MessageComponent',
+  props: {
+    msg: {
+      type: String,
+      default: ''
+    },
+    type: {
+      type: String,
+      default: 'success'
+    }
+  }
+}
+</script>
+
+<style scoped>
+.message {
+  padding: 12px;
+  border-radius: 5px;
+  margin: 16px auto;
+  max-width: 450px;
+  position: fixed;
+  top: 20px;
+  left: 50%;
+  transform: translateX(-50%);
+  z-index: 9999;
+}
+
+.success {
+  background-color: #c3e6cb;
+  border: 2px solid #8edd8e;
+  color: #155724;
+}
+
+.alert {
+  background-color: #f8d7da;
+  border: 2px solid #f5c6cb;
+  color: #721c24;
+}
+</style>
